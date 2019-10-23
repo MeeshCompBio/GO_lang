bacc928cd53e3dc945d82f020a772d2ff097d91d
Meesh <jean-michel.michno@chrobinson.com>
2019-10-23 13:47:24 -0500

    functions and imports

---
+++ tour_golang/function.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// notice that the type is declared after the var
+// if all the var types are the same, you can omit all but last delaration
+func add(x int, y int) int {
+	return x + y
+}
+
+// A funtion can return any number of resutls
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	fmt.Println(add(42, 99))
+	fmt.Println(swap("Hello", "Goodbye"))
+}
---
+++ tour_golang/package_import.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Need to use Capital letter to import, not lower case
+func main() {
+	fmt.Printf("Now you have %g problems \n", math.Sqrt(7))
+}
